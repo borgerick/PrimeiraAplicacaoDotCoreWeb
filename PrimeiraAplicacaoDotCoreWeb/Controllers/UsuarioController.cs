075b3399cca8a68bcc35684cce14b8983f23d1ac
ERICKBORGESRODRIGUES <ERICKBORGESRODRIGUES@SCS217791>
2025-07-23 22:06:11 -0300

    ADD PROJECT

---
+++ PrimeiraAplicacaoDotCoreWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraAplicacaoDotCoreWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NovoUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult PersistirUsuario( string nome, string sobrenome, string email)
+        {
+            return RedirectToAction("Index"); // Redireciona para a ação Index após persistir o usuário
+        }
+    }
+}
---
+++ Service/Db.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Service
+{
+    public class Db
+    {
+        private readonly MySqlConnection _con;
+        private MySqlCommand? _command;
+        private MySqlDataReader? _reader;
+
+        public Db()
+        {
+            _con = new MySqlConnection("Server=127.0.0.1;Database=cadusuario;Uid=root;Pwd=SslMode=none;");
+        }
+
+        public List<UsuarioDTO> GetData()
+        {
+            _con.Open();
+            _command = new MySqlCommand();
+            _command.Connection = _con;
+            _command.CommandText = "SELECT * FROM usuario";
+            _reader = _command.ExecuteReader();
+
+            List<UsuarioDTO> ListaDeUsuarios = new List<UsuarioDTO>();
+
+            while(_reader.Read())
+            {
+                var usuarioDTO = new UsuarioDTO()
+                {
+                    id = int.Parse(_reader["Id"].ToString()!),
+                    nome = _reader["Nome"].ToString()!,
+                    sobrenome = _reader["Sobrenome"].ToString()!,
+                    email = _reader["Email"].ToString()!,
+                };
+                ListaDeUsuarios.Add(usuarioDTO);
+            }
+
+            return ListaDeUsuarios;
+        }
+        public void AddUsuario()
+        {
+            _con.Open();
+            _command = new MySqlCommand();
+            _command.Connection = _con;
+
+            _command.CommandText = "INSERT INTO usuario (Nome, Sobrenome, Email) VALUES (?nome, ?sobrenome, ?email)";
+        }
+    }
+
+}
